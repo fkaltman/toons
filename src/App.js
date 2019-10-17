8121105ea4ab4abe356cd3b49b24e39a3b6a8aee
Francine Altman <francinekline@gmail.com>
2019-10-17 15:42:55 -0400

    no data from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Notes from './img/notes.png';
+import { findToons } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      venues: []
+    }
+  }
+
+  handleSearch = async (location) => {
+    const venues = await findToons(location);
+    console.log(venues);
+    this.setState({ venues });
+  }
+
+  render() {
+    return (
+      <div>
+        <img className="notes" src={Notes} alt="" />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const apiKey = 'Cevgw4CyvPl6GgL8jxkSMOWQ30JKK573VFf7QVheFnLyIHyn6hxrAPYSvkHhAAYeQLYOFH3gfkbwxKySuzU8jXx3BV9PgABgAp4WeHlT1aronHy7KfVhCuj-niQVXXYx'
+
+const baseURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search"
+
+const opts = {
+  headers: {
+    Authorization: `Bearer ${apiKey}`
+  }
+}
+
+export const findToons = async (location) => {
+  const response = await axios.get(`${baseURL}?categories=musicvenues&location=${location}`, opts);
+  return response.data.businesses
+}
